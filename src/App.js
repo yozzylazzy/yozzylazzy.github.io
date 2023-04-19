ca776c45887accd26627f91199f1e0aaf0234830
Yozzy Lazzy <kristian_yosef@yahoo.com>
2023-04-20 00:27:33 +0700

    Update Main App.JS Tes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 import CarousellImage from './section/home/carousell-image';
 import SvgLineDrawing from './VirtualCharaDrawed';
+import anime from 'animejs';
+
+const AnimeImage = () => {
+  const imageRef = useRef(null);
+  useEffect(() => {
+    const imageAnimation = anime({
+      targets: imageRef.current,
+      loop: true,
+      easing: 'linear',
+      opacity: [
+        { value: 0.8, duration: 100 },
+        { value: 0.3, duration: 200 },
+        { value: 0.8, duration: 200 }
+      ],
+      scale: [
+        { value: 1.05, duration: 100 },
+        { value: 1.1, duration: 200 },
+        { value: 1, duration: 200 }
+      ]
+    });
+
+    return () => {
+      imageAnimation.pause();
+    }
+  }, []);
+
+  return (
+    <img src={logo} className="App-logo" alt="logo" ref={imageRef} />
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <CarousellImage />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <SvgLineDrawing />
+        <AnimeImage/>
         <p>
           Portofolio Web On Development
         </p>
